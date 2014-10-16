fd20a8b9262131dbbac849265fafc39fbc680fa5
Jason Felds <jafelds@gmail.com>
2014-10-15 22:20:28 -0400

    Demonstrate generating files.

--- tutorial.cpp
+++ tutorial.cpp
@@ -2,11 +2,13 @@
 
 #include <iostream>
 #include <cmath>
+#include "TutorialConfig.hpp"
 using std::cout;
 using std::endl;
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
+    cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << endl;
     cout << "Usage: " << argv[0] << " number" << endl;
     return 1;
   }
